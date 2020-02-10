37e9fa08274fdb2d964baca312661f9640648a20
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2020-02-10 08:44:26 +0100

    Update pestle.rb
    Updated to peste 1.5.2

--- Formula/pestle.rb
+++ Formula/pestle.rb
@@ -1,13 +1,13 @@
 class Pestle < Formula
   desc "A collection of command line scripts for Magento 2 code generation, and a PHP module system for organizing command line scripts."
   homepage "https://github.com/astorm/pestle"
-  url "https://fabrizioballiano.com/pestle/pestle-1.5.0.phar"
-  sha256 "ca4e57ceef400cd5b7a5674f3df3db14cdd79d51ab94e83f834668d23f23b310"
+  url "http://pestle.pulsestorm.net/pestle-1-5-2.phar"
+  sha256 "4796f9def1f0c08c646f6b9625f417134dcdc2a2ec34514bdc718d71b35c475b"
 
   bottle :unneeded
 
   def install
-    bin.install "pestle-1.5.0.phar" => "pestle"
+    bin.install "pestle-1.5.2.phar" => "pestle"
   end
 
   test do
