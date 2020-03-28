996c240c87c22c8ed39be3becbc55e233423c88e
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2020-03-28 10:16:00 +0000

    magerun updated to 1.103.2

--- Formula/magerun.rb
+++ Formula/magerun.rb
@@ -1,13 +1,13 @@
 class Magerun < Formula
   desc "Swiss army knife for Magento developers, sysadmins and devops."
   homepage "http://magerun.net/"
-  url "https://files.magerun.net/n98-magerun-1.103.1.phar"
-  sha256 "f4de50f5e7f9db70ee82148339ca865f14b7cdf7713d1f7c9357b84067235ce6"
+  url "https://files.magerun.net/n98-magerun-1.103.2.phar"
+  sha256 "54eaf7cc7385fdccd838fdae7439dbb7c689110cbf714ab567f7089f78517b67"
 
   bottle :unneeded
 
   def install
-    bin.install "n98-magerun-1.103.1.phar" => "magerun"
+    bin.install "n98-magerun-1.103.2.phar" => "magerun"
   end
 
   test do
