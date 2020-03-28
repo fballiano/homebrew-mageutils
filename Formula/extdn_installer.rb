f35b18a667f2daffbc5f776f6c75294d55bdd788
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2020-03-28 10:21:17 +0000

    version metatag was added

--- Formula/extdn_installer.rb
+++ Formula/extdn_installer.rb
@@ -1,6 +1,7 @@
 class ExtdnInstaller < Formula
   desc "Universal extension installer for Magento 2."
   homepage "https://github.com/extdn/installer-m2"
+  version "1.0.0-beta6"
   url "https://github.com/extdn/installer-m2/releases/download/v1.0.0-beta6/extdn_installer.phar"
   sha256 "9dc269d30c864a7bffbfaff74a8ba614e24a92dfc9f33d1ce0bae7c02a2f5d5a"
 
