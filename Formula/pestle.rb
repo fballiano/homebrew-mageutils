1f3f245127c569a7b0b173fde8afa184856757b9
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2021-03-22 22:48:26 +0000

    updated to 1.5.3

--- Formula/pestle.rb
+++ Formula/pestle.rb
@@ -1,7 +1,7 @@
 class Pestle < Formula
   desc "A collection of command line scripts for Magento 2 code generation, and a PHP module system for organizing command line scripts."
   homepage "https://github.com/astorm/pestle"
-  version "1.5.2"
+  version "1.5.3"
   url "http://pestle.pulsestorm.net/pestle-1-5-3.phar"
   sha256 "0e7a31d54cf4d82a1878e9b4fb12593c357f8f24b2fab5788271fce0b6a40930"
 
