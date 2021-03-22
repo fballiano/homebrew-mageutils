3bee4dfdeb85e9d445f640eedb6286cca23725bc
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2021-03-22 22:45:26 +0000

    typo

--- Formula/magerun2.rb
+++ Formula/magerun2.rb
@@ -1,7 +1,7 @@
 class Magerun2 < Formula
   desc "Swiss army knife for Magento developers, sysadmins and devops."
   homepage "http://magerun.net/"
-  url "https://files.magerun.net/n98-magerun2-4.2.0.phar"
+  url "https://files.magerun.net/n98-magerun2-4.5.0.phar"
   sha256 "236a50552f3bfa9e02b57e064a49b1dd422d6b21821b55a21bfe4ac2f6e7a445"
 
   bottle :unneeded
