c5a4414fa511415f5647c058c4ef1c117b75ea47
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2021-03-22 22:47:08 +0000

    updated to 1.5.3

--- Formula/pestle.rb
+++ Formula/pestle.rb
@@ -2,13 +2,13 @@ class Pestle < Formula
   desc "A collection of command line scripts for Magento 2 code generation, and a PHP module system for organizing command line scripts."
   homepage "https://github.com/astorm/pestle"
   version "1.5.2"
-  url "http://pestle.pulsestorm.net/pestle-1-5-2.phar"
-  sha256 "4796f9def1f0c08c646f6b9625f417134dcdc2a2ec34514bdc718d71b35c475b"
+  url "http://pestle.pulsestorm.net/pestle-1-5-3.phar"
+  sha256 "0e7a31d54cf4d82a1878e9b4fb12593c357f8f24b2fab5788271fce0b6a40930"
 
   bottle :unneeded
 
   def install
-    bin.install "pestle-1-5-2.phar" => "pestle"
+    bin.install "pestle-1-5-3.phar" => "pestle"
   end
 
   test do
