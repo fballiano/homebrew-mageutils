c07d3e8839a83b7029316c031db29c21d7290a30
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2021-10-20 11:40:03 +0100

    Update pestle.rb
    removed bottle unneeded

--- Formula/pestle.rb
+++ Formula/pestle.rb
@@ -5,8 +5,6 @@ class Pestle < Formula
   url "http://pestle.pulsestorm.net/pestle-1-5-3.phar"
   sha256 "0e7a31d54cf4d82a1878e9b4fb12593c357f8f24b2fab5788271fce0b6a40930"
 
-  bottle :unneeded
-
   def install
     bin.install "pestle-1-5-3.phar" => "pestle"
   end
