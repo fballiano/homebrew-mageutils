75b3a0e4783be740e25db69d503758d38f400062
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2021-10-20 11:40:23 +0100

    Update magerun.rb
    removed bottle unneeded

--- Formula/magerun.rb
+++ Formula/magerun.rb
@@ -4,8 +4,6 @@ class Magerun < Formula
   url "https://files.magerun.net/n98-magerun-2.2.0.phar"
   sha256 "7dd1db19859ad7b05bbaa7fe58295fceb39c3378daeadda17bdc9ae796ba59f3"
 
-  bottle :unneeded
-
   def install
     bin.install "n98-magerun-2.2.0.phar" => "magerun"
   end
