1538e4320aaa7280bff42ff76d87a12f9106a494
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2021-10-20 11:40:13 +0100

    Update extdn_installer.rb
    removed bottle unneeded

--- Formula/extdn_installer.rb
+++ Formula/extdn_installer.rb
@@ -5,8 +5,6 @@ class ExtdnInstaller < Formula
   url "https://github.com/extdn/installer-m2/releases/download/v1.0.0-beta6/extdn_installer.phar"
   sha256 "9dc269d30c864a7bffbfaff74a8ba614e24a92dfc9f33d1ce0bae7c02a2f5d5a"
 
-  bottle :unneeded
-
   def install
     bin.install "extdn_installer.phar" => "extdn_installer"
   end
