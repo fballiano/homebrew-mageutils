4814ae1bc571134aebd8d123cdf004eec088dfcd
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2021-12-05 09:24:12 +0000

    4.8.0

--- Formula/magerun2.rb
+++ Formula/magerun2.rb
@@ -1,11 +1,11 @@
 class Magerun2 < Formula
   desc "Swiss army knife for Magento developers, sysadmins and devops."
   homepage "http://magerun.net/"
-  url "https://files.magerun.net/n98-magerun2-4.7.0.phar"
-  sha256 "4bf8ca60e5a05afafba700f89466b95a56b1bae4110fda3706c9c2e26f952ae4"
+  url "https://files.magerun.net/n98-magerun2-4.8.0.phar"
+  sha256 "c23fcae2057ffc770828bb223da322eaf8602efbe5898cf2be1e5a252bec76fb"
 
   def install
-    bin.install "n98-magerun2-4.7.0.phar" => "magerun2"
+    bin.install "n98-magerun2-4.8.0.phar" => "magerun2"
   end
 
   test do
