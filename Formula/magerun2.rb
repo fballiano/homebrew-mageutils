3c37cbd861bac4f605cda5a6415726c29c697749
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-04-30 19:04:55 +0100

    updated to 5.0.2

--- Formula/magerun2.rb
+++ Formula/magerun2.rb
@@ -1,11 +1,11 @@
 class Magerun2 < Formula
   desc "Swiss army knife for Magento developers, sysadmins and devops."
   homepage "http://magerun.net/"
-  url "https://files.magerun.net/n98-magerun2-4.8.0.phar"
-  sha256 "c23fcae2057ffc770828bb223da322eaf8602efbe5898cf2be1e5a252bec76fb"
+  url "https://files.magerun.net/n98-magerun2-5.0.2.phar"
+  sha256 "5de32880acb962d3d47839ba5e9b37c2a94409a116e6e7c82929cdc8c802708d"
 
   def install
-    bin.install "n98-magerun2-4.8.0.phar" => "magerun2"
+    bin.install "n98-magerun2-5.0.2.phar" => "magerun2"
   end
 
   test do
