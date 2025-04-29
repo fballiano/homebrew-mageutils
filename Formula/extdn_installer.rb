fc0eca22998e6b9d49a39f76c2c6a1e0979b1642
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2025-04-29 10:29:40 +0100

    Update extdn_installer.rb

--- Formula/extdn_installer.rb
+++ Formula/extdn_installer.rb
@@ -1,9 +1,9 @@
 class ExtdnInstaller < Formula
   desc "Universal extension installer for Magento 2."
   homepage "https://github.com/extdn/installer-m2"
-  version "1.0.0-beta6"
-  url "https://github.com/extdn/installer-m2/releases/download/v1.0.0-beta6/extdn_installer.phar"
-  sha256 "9dc269d30c864a7bffbfaff74a8ba614e24a92dfc9f33d1ce0bae7c02a2f5d5a"
+  version "1.0.0-rc6"
+  url "https://github.com/extdn/installer-m2/releases/download/v1.0.0-rc6/extdn_installer.phar"
+  sha256 "c3cd30ca21cd19e4994d998fd5ef946285bbad5cb31aeff4f4491c310235db15"
 
   def install
     bin.install "extdn_installer.phar" => "extdn_installer"
